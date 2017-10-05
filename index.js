e85837da2d2244152fe8b62efd193fc42461e932
Matthias Lohmeier <matthias.lohmeier@signavio.com>
2017-10-05 14:17:28 +0200

    utterance

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ alexaApp.intent("to", {
         var tweet = request.slot("TWEET");
         console.log('received the following text: ' + tweet)
         sendMail(tweet, response);
-        response.say("Success! I retrieved your tweet " + tweet);
+        // response.say("Success! I retrieved your tweet " + tweet);
     }
 );
 
